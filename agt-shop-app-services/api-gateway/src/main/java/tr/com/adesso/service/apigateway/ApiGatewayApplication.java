18976f1ee54d19b79fe754cafd0cc75563e88eb0
Berkay Yalçın <kemalberkayyalcin@gmail.com>
2023-07-18 12:30:54 +0300

    config server implemented.Configurations of services migrated to config-server recources

--- agt-shop-app-services/api-gateway/src/main/java/tr/com/adesso/service/apigateway/ApiGatewayApplication.java
+++ agt-shop-app-services/api-gateway/src/main/java/tr/com/adesso/service/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package tr.com.adesso.service.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
